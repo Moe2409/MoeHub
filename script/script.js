476875b1afe15dacf7a4237c7ef351996d269bb5
Moe <LeonEnMoe@gmail.com>
2026-01-14 01:17:17 +0100

    added a variety of dailyComments

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
-function fuck(s) {
-    console.log(s)
-}
+const dailyComments = [
+    '"Have a great day"',
+    '"You got this"',
+    '"I believe in you"',
+    `"You're doing great"`,
+    '"Keep going"',
+    '"One step at a time"',
+    '"Take it easy today"'
+];
+
+$(document).ready(function () {
+    const random = Math.floor(Math.random() * dailyComments.length);
+    $("#sucroseWriting").text(dailyComments[random]);
+});
