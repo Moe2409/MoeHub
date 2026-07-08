3319297c8a4e0845f5f79e1b08b0d1483db2f6fc
Moe <LeonEnMoe@gmail.com>
2026-07-08 17:34:24 +0200

    updated a lot of design and fixed expand/compress problem

--- script/to-do-list.js
+++ script/to-do-list.js
@@ -7,9 +7,6 @@ $(document).ready(function () {
 
             const progressPercent = totalTasks > 0 ? Math.round((completedTasks/totalTasks)*100) : 0;
 
-            const nextTask = project.tasks.find(t => t.status !== "completed") || {id : "none", title: "No More Tasks", status: "completed"};
-            const isChecked = nextTask.status === "completed" ? "checked" : "";
-
             const allTasksHTML = project.tasks.map(function(task) {
                 const taskChecked = task.status === "completed" ? "checked" : "";
 
@@ -31,15 +28,8 @@ $(document).ready(function () {
                         </div>
                         <span class="progressText">${progressPercent}%</span>
                     </div>
-                    
-                    <h3 class="nextTaskHeader">Next Task:</h3>
-                    <div class="nextTaskContainer">
-                        <input id="${nextTask.id}" type="checkbox" ${isChecked}>
-                        <label class="nextTaskLabel" for="${nextTask.id}">${nextTask.title}</label>
-                    </div>
 
                     <div class="tasksContainer contenteditable="true"">
-                        <h4>All Tasks:</h4>
                         ${allTasksHTML}
                     </div>
                 </article>
@@ -53,7 +43,7 @@ $(document).ready(function () {
     $("#projectsContainer").on("click", ".project", function() {
         $(this).find(".tasksContainer").slideToggle();
     });
-    $("#projectsContainer").on("click", ".nextTaskContainer input", function(event) {
-        
+    $("#projectsContainer").on("click", ".taskCheckbox, .taskLabel", function(event) {
+        event.stopPropagation();
     });
 });
